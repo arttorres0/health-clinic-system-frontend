eff00909dfaa127df8ffbd1fa2ff45ad9707c013
Arthur Torres <arttorres2004@hotmail.com>
2020-03-07 16:53:58 -0400

    Change from css to scss; add sidebar, navbar and page-container initial structure

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pages',
-  templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.css']
+  selector: "app-pages",
+  templateUrl: "./pages.component.html",
+  styleUrls: ["./pages.component.scss"]
 })
 export class PagesComponent implements OnInit {
+  private sidebarIsHidden: boolean;
+  private isMobileSidebar: boolean;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.setSidebarAttrs(window.innerWidth);
   }
 
+  onResize(): void {
+    this.setSidebarAttrs(window.innerWidth);
+  }
+
+  setSidebarAttrs(width): void {
+    if (width > 991) {
+      this.sidebarIsHidden = false;
+      this.isMobileSidebar = false;
+    } else {
+      this.sidebarIsHidden = true;
+      this.isMobileSidebar = true;
+    }
+  }
+
+  showSidebar(): void {
+    this.sidebarIsHidden = false;
+  }
+
+  hideSidebar(): void {
+    this.sidebarIsHidden = true;
+  }
 }
